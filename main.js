b6410292a16336d14494432c84124b537256baa8
eguchishoichi <75185675+eguchishoichi@users.noreply.github.com>
2021-02-09 11:58:08 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$('.btn-bottom').click(function() {
+  $("html,body").animate({scrollTop:$("#home").offset().top});
+});
+
+$('.animated').waypoint({
+  handler(direction) {
+    // 要素が画面中央に来るとここが実行される
+    if (direction === 'down') {
+      /**
+       * 下方向のスクロール
+       * イベント発生元の要素にfadeInUpクラスを付けることで
+       * アニメーションを開始
+       */
+      $(this.element).addClass('fadeInUp');
+
+      /**
+       * waypointを削除することで、この要素に対しては
+       * これ以降handlerが呼ばれなくなる
+       */
+      this.destroy();
+    }
+  },
+
+  // 要素が画面中央に来たらhandlerを実行
+  offset: '50%',
+});
+
